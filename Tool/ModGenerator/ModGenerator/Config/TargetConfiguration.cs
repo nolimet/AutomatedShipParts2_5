a2b70ab02246af4f073329c003b8f5dbfc528708
jesse stam <nolimet@stamdata.nl>
2025-08-04 16:36:41 +0200

    Part one done

--- Tool/ModGenerator/ModGenerator/Config/TargetConfiguration.cs
+++ Tool/ModGenerator/ModGenerator/Config/TargetConfiguration.cs
@@ -1,10 +1,23 @@
-﻿namespace ModGenerator.Config;
+﻿using Newtonsoft.Json;
 
-public class TargetConfiguration(string baseGamePath, string[] vanillaParts, string baseModPath, Dictionary<long, string> targetComponents)
+namespace ModGenerator.Config;
+
+public class TargetConfiguration
 {
-    public readonly string BaseGamePath = baseGamePath;
-    public readonly string[] VanillaParts = vanillaParts;
+    public readonly string BaseGamePath;
+    public readonly string[] VanillaBlackList;
 
-    public readonly string BaseModPath = baseModPath;
-    public readonly Dictionary<long, string> TargetComponents = targetComponents;
+    public readonly string BaseModPath;
+    public readonly Dictionary<long, string[]> TargetComponents;
+
+    [JsonConstructor]
+    public TargetConfiguration(string baseGamePath, string[] vanillaBlackList, string baseModPath, Dictionary<long, string[]> targetComponents)
+    {
+        BaseGamePath = baseGamePath;
+        VanillaBlackList = vanillaBlackList;
+        BaseModPath = baseModPath;
+        TargetComponents = targetComponents;
+    }
+
+    public TargetConfiguration() : this(string.Empty, [], string.Empty, new Dictionary<long, string[]>()) { }
 }
